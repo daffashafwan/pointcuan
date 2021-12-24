6f085c6cae5a1f6f20b2604917b999bbdab3c473
nurdin-sapariansyah <nurdin27muhammad@gmail.com>
2021-12-24 08:10:58 +0800

    menambahkan bussiness logic untuk pcr crud admin

---
+++ business/pcr_crud/domain.go
@@ -0,0 +1,22 @@
+package pcrcrud
+
+import (
+	"context"
+	"time"
+)
+
+type Domain struct {
+	Id   int
+	NilaiPcr int
+	UpdatedAt time.Time 
+}
+
+type Usecase interface {
+	Update(ctx context.Context, domain Domain) (Domain, error)
+	GetById(ctx context.Context, id int) (Domain, error)
+}
+
+type Repository interface {
+	Update(ctx context.Context, domain Domain) (Domain, error)
+	GetById(ctx context.Context, id int) (Domain, error)
+}
