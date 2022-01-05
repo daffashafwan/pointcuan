a81eb28713196d5d5e4ee13281ba69f97779a867
nurdin-sapariansyah <nurdin27muhammad@gmail.com>
2022-01-05 13:07:53 +0800

    menambahkan bussiness untuk item

---
+++ business/items/domain.go
@@ -0,0 +1,32 @@
+package items
+
+import (
+	"context"
+	"time"
+)
+
+type Domain struct {
+	Id          int
+	CategoryId  int
+	Name        string
+	PointRedeem int
+	Stock       int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Usecase interface {
+	Create(ctx context.Context, domain Domain) (Domain, error)
+	Update(ctx context.Context, domain Domain, id int) (Domain, error)
+	GetAll(ctx context.Context) ([]Domain, error)
+	GetByItemId(ctx context.Context, id int) (Domain, error)
+	Delete(ctx context.Context, id int) ( error)
+}
+
+type Repository interface {
+	Create(ctx context.Context, domain *Domain) (Domain, error)
+	Update(ctx context.Context, domain Domain) (Domain, error)
+	GetAll(ctx context.Context) ([]Domain, error)
+	GetByItemId(ctx context.Context, id int) (Domain, error)
+	Delete(ctx context.Context, id int) ( error)
+}
---
+++ business/items/usecase.go
@@ -0,0 +1,70 @@
+package items
+
+import (
+	"context"
+	"errors"
+	"time"
+
+)
+
+type ItemsUsecase struct {
+	Repo           Repository
+	contextTimeout time.Duration
+}
+
+func NewItemsUsecase(repo Repository, timeout time.Duration) Usecase {
+	return &ItemsUsecase{
+		Repo:           repo,
+		contextTimeout: timeout,
+	}
+}
+
+
+func (ic *ItemsUsecase) Create(ctx context.Context, domain Domain) (Domain, error) {
+	var err error
+	items, err := ic.Repo.Create(ctx, &domain)
+	if err != nil {
+		return Domain{}, err
+	}
+
+	return items, nil
+}
+
+func (ic *ItemsUsecase) Delete(ctx context.Context, id int) ( error) {
+	err := ic.Repo.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ic *ItemsUsecase) GetAll(ctx context.Context) ([]Domain, error) {
+	items, err := ic.Repo.GetAll(ctx)
+	if err != nil {
+		return []Domain{}, err
+	}
+	return items, nil
+}
+
+func (ic *ItemsUsecase) GetByItemId(ctx context.Context, id int) (Domain, error) {
+	items, err := ic.Repo.GetByItemId(ctx, id)
+	if err != nil {
+		return Domain{}, err
+	}
+	if items.Id == 0 {
+		return Domain{}, errors.New("ID NOT FOUND")
+	}
+	return items, nil
+}
+
+func (ic *ItemsUsecase) Update(ctx context.Context, domain Domain, id int) (Domain, error) {
+	domain.Id = id
+	items, err := ic.Repo.Update(ctx, domain)
+
+	if err != nil {
+		return Domain{}, err
+	}
+
+	return items, nil
+}
+
