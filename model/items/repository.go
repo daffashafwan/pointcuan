5fff01b38489f2b9b44ff0c67ca5be094ce86a06
nurdin-sapariansyah <nurdin27muhammad@gmail.com>
2022-01-05 13:38:57 +0800

    menambahkan model untuk item

---
+++ model/items/repository.go
@@ -0,0 +1,76 @@
+package items
+
+import (
+	"context"
+	"errors"
+
+	"github.com/daffashafwan/pointcuan/business/items"
+	"gorm.io/gorm"
+)
+
+type ItemsRepo struct {
+	DB *gorm.DB
+}
+
+func CreateItemRepo(conn *gorm.DB) items.Repository {
+	return &ItemsRepo{
+		DB: conn,
+	}
+}
+
+func (rep *ItemsRepo) Create(ctx context.Context,itemCreate *items.Domain) (items.Domain, error) {
+	item := Items{
+		CategoryId: itemCreate.CategoryId,
+		Name: itemCreate.Name,
+		PointRedeem: itemCreate.PointRedeem,
+		Stock: itemCreate.Stock,
+	}
+	err := rep.DB.Create(&item)
+	if err.Error != nil {
+		return items.Domain{}, err.Error
+	}
+	return item.ToDomain(), nil
+}
+
+func (repo *ItemsRepo) Update(ctx context.Context, itemUpdate items.Domain) (items.Domain, error) {
+	data := FromDomain(itemUpdate)
+	err := repo.DB.Table("items").First(&data)
+	if err.Error != nil {
+		return items.Domain{}, err.Error
+	}
+	data.Items = itemUpdate.Items
+	if repo.DB.Save(&data).Error != nil {
+		return items.Domain{}, errors.New("bad requests")
+	}
+	return data.ToDomain(), nil
+}
+
+func (repo *ItemsRepo) GetAll(ctx context.Context) ([]items.Domain, error) {
+	var data []Items
+	err := repo.DB.Table("items").Find(&data)
+	if err.Error != nil {
+		return []items.Domain{}, err.Error
+	}
+	return ToListDomain(data), nil
+}
+
+func (repo *ItemsRepo) GetByItemId(ctx context.Context, id int) (items.Domain, error) {
+	var data Items
+	err := repo.DB.Table("items").Find(&data, "item_id=?", id)
+	if err.Error != nil {
+		return items.Domain{}, err.Error
+	}
+	return data.ToDomain(), nil
+}
+
+func (repo *ItemsRepo) Delete(ctx context.Context, id int) error {
+	item := Items{}
+	err := repo.DB.Table("items").Where("item_id = ?", id).First(&item).Delete(&item)
+	if err.Error != nil {
+		return err.Error
+	}
+	if err.RowsAffected == 0 {
+		return errors.New("id not found")
+	}
+	return nil
+}
